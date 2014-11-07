a117631fd1f607cc3474ed9c6368f1b9fea68bfa
Cole Busby <colebusby@gmail.com>
2014-11-06 17:23:06 -0700

    disabled spree mailer

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,6 +9,7 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
+  config.override_actionmailer_config = false
 end
 
 Spree.user_class = "Spree::User"
